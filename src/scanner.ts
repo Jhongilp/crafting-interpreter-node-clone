d751a52d0f193b456bc01f58b0b91c217bb5ca14
Jonathan Gil <jhongilp@gmail.com>
2021-12-02 20:30:12 -0500

    handle cases for identifiers, number or literal strings

--- src/scanner.ts
+++ src/scanner.ts
@@ -8,6 +8,25 @@ export class Scanner {
   private current = 0;
   private line = 1;
 
+  static keywords: { [key: string]: TokenType } = {
+    and: "AND",
+    class: "CLASS",
+    else: "ELSE",
+    false: "FALSE",
+    for: "FOR",
+    fun: "FUN",
+    if: "IF",
+    nil: "NIL",
+    or: "OR",
+    print: "PRINT",
+    return: "RETURN",
+    super: "SUPER",
+    this: "THIS",
+    true: "TRUE",
+    var: "VAR",
+    while: "WHILE",
+  };
+
   constructor(source: string) {
     this.source = source;
   }
@@ -90,14 +109,17 @@ export class Scanner {
       case "\n":
         this.line++;
         break;
+      case '"':
+        this.string();
+        break;
       default:
-        // if (isDigit(c)) {
-        //   number();
-        // } else if (isAlpha(c)) {
-        //   identifier();
-        // } else {
-        //   IntroTest.error(line, "Unexpected character.");
-        // }
+        if (this.isDigit(c)) {
+          this.number();
+        } else if (this.isAlpha(c)) {
+          this.identifier();
+        } else {
+          console.error("Unexpected character. ", this.line);
+        }
 
         break;
     }
@@ -105,12 +127,6 @@ export class Scanner {
 
   advance() {
     return this.source.charAt(this.current++);
-  }
-
-  addToken(type: TokenType, literal: null | {}) {
-    const lexeme = this.source.substring(this.start, this.current);
-    console.log("[add_token] lexeme: ", lexeme);
-    this.tokens.push(Token({ type, lexeme, literal, line: this.line }));
   }
 
   match(expected: string) {
@@ -132,8 +148,87 @@ export class Scanner {
     return this.source.charAt(this.current);
   }
 
+  peekNext() {
+    if (this.current + 1 >= this.source.length) {
+      return "\0";
+    }
+    return this.source.charAt(this.current + 1);
+  }
+
+  identifier() {
+    while (this.isAlphaNumeric(this.peek())) {
+      this.advance();
+    }
+
+    const text = this.source.substring(this.start, this.current);
+    let type = Scanner.keywords[text];
+    if (!type) {
+      type = "IDENTIFIER";
+    }
+    this.addToken(type, null);
+  }
+
+  number() {
+    while (this.isDigit(this.peek())) {
+      this.advance();
+    }
+
+    // Look for a fractional part.
+    if (this.peek() == "." && this.isDigit(this.peekNext())) {
+      // Consume the "."
+      this.advance();
+
+      while (this.isDigit(this.peek())) {
+        this.advance();
+      }
+    }
+
+    this.addToken(
+      "NUMBER",
+      parseFloat(this.source.substring(this.start, this.current))
+    );
+  }
+
+  string() {
+    while (this.peek() != '"' && !this.isAtEnd()) {
+      if (this.peek() == "\n") {
+        this.line++;
+      }
+      this.advance();
+    }
+
+    if (this.isAtEnd()) {
+      console.error("Unterminated string. ", this.line);
+      return;
+    }
+
+    // The closing ".
+    this.advance();
+
+    // Trim the surrounding quotes.
+    const value = this.source.substring(this.start + 1, this.current - 1);
+    this.addToken("STRING", value);
+  }
+
+  isAlpha(c: string) {
+    return (c >= "a" && c <= "z") || (c >= "A" && c <= "Z") || c == "_";
+  }
+
+  isAlphaNumeric(c: string) {
+    return this.isAlpha(c) || this.isDigit(c);
+  }
+
+  isDigit(c: string) {
+    return c >= "0" && c <= "9";
+  }
+
+  addToken(type: TokenType, literal: null | string | number) {
+    const lexeme = this.source.substring(this.start, this.current);
+    console.log("[add_token] lexeme: ", lexeme);
+    this.tokens.push(Token({ type, lexeme, literal, line: this.line }));
+  }
+
   test() {
-    // const token = TokenType.AND;
     console.log("Lox interpreter source: ", this.source);
     console.log("Lox interpreter tokens: ", this.tokens);
   }
